05eaa24e466c468abdd7dfcbaf54881037d8e8b1
Sebastys <xspania@gmail.com>
2025-08-29 14:06:19 +0200

    change of region

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "location" {
   description = "The Azure Region in which all resources should be created"
   type        = string
-  default     = "East US"
+  default     = "West Europe"
 }
 
 variable "admin_username" {
